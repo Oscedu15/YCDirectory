81a0c79fb34fbb10582992b6b2e64a4b54179820
Oscar Garcia <oscaregarcialeon@gmail.com>
2024-12-29 17:36:12 -0400

    Cache and Live API w/ Next.js

--- sanity/lib/client.ts
+++ sanity/lib/client.ts
@@ -6,5 +6,7 @@ export const client = createClient({
   projectId,
   dataset,
   apiVersion,
-  useCdn: true, // Set to false if statically generating pages, using ISR or tag-based revalidation
+  useCdn: false, // Set to false if statically generating pages, using ISR or tag-based revalidation
+  //!Al estar useCdn en false, obtendremos los resultados directamente desde la base de datos y no del cache
 })
+//?Intsalamos npm i server-only, para garantizar que cualquier nueva informacion cargada en la base de datos, sea renderizada directamente
